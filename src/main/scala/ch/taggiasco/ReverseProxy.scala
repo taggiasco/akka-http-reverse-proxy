d91284092330b2cf8871ce7935efc3a02e7fd87f
Matthieu Taggiasco <matt@taggiasco.ch>
2016-08-13 13:26:15 +0200

    Monitoring actor added. Basic async handler

--- src/main/scala/ch/taggiasco/ReverseProxy.scala
+++ src/main/scala/ch/taggiasco/ReverseProxy.scala
@@ -1,6 +1,6 @@
 package ch.taggiasco
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, ActorRef}
 import akka.util.ByteString
 import akka.event.{Logging, LoggingAdapter}
 import akka.stream.ActorMaterializer
@@ -23,6 +23,18 @@ object ReverseProxy extends Config {
     
     val log: LoggingAdapter = Logging(system, getClass)
     
+    val monitoringActor: ActorRef = system.actorOf(MonitoringActor.props)
+    
+    val reactToTopLevelFailures = Flow[Http.IncomingConnection].watchTermination()((_, termination) => termination.onFailure {
+      case cause => monitoringActor ! cause
+    })
+    
+    val reactToConnectionFailure = Flow[HttpRequest].recover[HttpRequest] {
+      case ex =>
+        // handle the failure somehow
+        monitoringActor ! ex
+        throw ex
+    }
     
     val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] = Http().bind(interface = httpInterface, port = httpPort)
     
@@ -45,17 +57,24 @@ object ReverseProxy extends Config {
         HttpResponse(404, entity = "Unknown resource!")
     }
     
+    val asyncRequestHandler: HttpRequest => Future[HttpResponse] = {
+      httpRequest => Future.successful(requestHandler(httpRequest))
+    }
     
-    val binding: Future[Http.ServerBinding] = serverSource.to(
+    
+    val binding: Future[Http.ServerBinding] = serverSource.via(reactToTopLevelFailures).to(
       Sink.foreach { connection =>
         println("Accepted new connection from " + connection.remoteAddress)
-        connection handleWithSyncHandler requestHandler
+        connection handleWithAsyncHandler asyncRequestHandler
       }
     ).run()
     
     println(s"Server online at http://$httpInterface:$httpPort\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     
+    binding.onFailure{
+      case ex: Exception => log.error(ex, "Failed to bind to {}:{}!", httpInterface, httpPort)
+    }
     binding
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
