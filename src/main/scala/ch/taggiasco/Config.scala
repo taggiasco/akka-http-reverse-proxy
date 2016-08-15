f13a811774a28a0335eddc7be877311cd5f1078a
Matthieu Taggiasco <mtaggiasco@eqnx.ch>
2016-08-15 11:32:36 +0200

    Configuration changed and added first elements for reverse-proxy configuration

--- src/main/scala/ch/taggiasco/Config.scala
+++ src/main/scala/ch/taggiasco/Config.scala
@@ -5,7 +5,7 @@ import com.typesafe.config.ConfigFactory
 
 trait Config {
   private val config = ConfigFactory.load()
-  private val httpConfig = config.getConfig("http")
+  private val httpConfig = config.getConfig("reverse-proxy.http")
   
   val httpInterface = httpConfig.getString("interface")
   val httpPort = httpConfig.getInt("port")
